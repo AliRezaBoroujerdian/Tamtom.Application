70ea38b9e08e953af2db4a1e7ecf22f77206fa1a
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2020-02-25 12:09:13 +0330

    Create DataAccess and URI Class

---
+++ Tamtom/Tamtom.Application/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamtom.Application
+{
+    public static class DataAccess
+    {
+        public static string ConnectionString { get; private set; }
+
+        /// <summary>
+        /// Initialize connection string
+        /// </summary>
+        /// <param name="server">locol host, dot, 198.125.125.125, ...</param>
+        /// <param name="database">your database name</param>
+        /// <param name="user">database user id</param>
+        /// <param name="password">user password</param>
+        public static void Initialize(string server, string database, string user, string password)
+        {
+            ConnectionString = $"Server={server};Database={database};User Id={user};Password={password};";
+        }
+
+    }
+}
---
+++ Tamtom/Tamtom.Application/URI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamtom.Application
+{
+    /// <summary>
+    /// UniformResourceIdentifier
+    /// </summary>
+    public static class URI
+    {
+        private static string protocol;
+        private static string domain;
+
+        /// <summary>
+        /// UniformResourceLocator
+        /// </summary>
+        public static string URL { get; private set; }
+
+        /// <summary>
+        /// Initialize static url
+        /// </summary>
+        /// <param name="protocol">http, https or ftp</param>
+        /// <param name="domain">www.mysite.com</param>
+        public static void Initialize(string protocol, string domain)
+        {
+            if (!(protocol.ToLower() == "http" | protocol.ToLower() == "https" | protocol.ToLower() == "ftp"))
+                throw new ArgumentOutOfRangeException("Protocol", protocol, "http, https or ftp");
+
+            URI.protocol = protocol.ToLower();
+            URI.domain = domain.ToLower();
+
+            URL = URI.protocol + "://" + URI.domain;
+        }
+    }
+}
